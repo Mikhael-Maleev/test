b6214f256b0cc30d0da5978be3ccca0ee612d2c0
mmaleev <test@test.com>
2018-03-23 12:29:31 +0200

    test 14 master This is flomaster branch
    Signed-off-by: mmaleev <test@test.com>

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-print getcwd();
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * The routines here dispatch control to the appropriate handler, which then
+ * prints the appropriate page.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ */
 
-
+print 'Helloy world';
